277c2cb569f8d32617556fa4a146d2751f15eace
Mikail Sayan <msayan@outlook.de>
2022-04-26 14:39:33 +0200

    Add Headerbar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,13 @@
+import { Container } from "@mui/material";
 import Header from "./Header";
 
-export default function Layout({children}) {
-
-    return (
-        <>
-            <Header />
-            {children}
-        </>
-    ) 
+export default function Layout({ children }) {
+  return (
+    <>
+      <Header />
+      <Container maxWidth="xl" sx={{ marginTop: "6rem" }}>
+        {children}
+      </Container>
+    </>
+  );
 }

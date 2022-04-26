1a617e71d425e6719cdf74ee36d2c85a71d8e2ff
Mikail Sayan <msayan@outlook.de>
2022-04-26 12:44:53 +0200

    Delete Styled

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,16 @@
 import Link from "next/link";
-import styled from "styled-components";
 
 export default function Header() {
 
     return (
-        <HeaderStyle>
+        <header>
             <Link href="/">Home</Link>
             <br />
             <Link href="/cards">Cards</Link>
             <br />
             <Link href="/create">Create</Link>
             <br />
-        </HeaderStyle>
+        </header>
     );
 }
 
-const HeaderStyle = styled.header`
-    background-color: green;
-`

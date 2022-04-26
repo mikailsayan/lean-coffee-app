1e961bcabef82f39a9c81b5b0b62eceb376b4d1d
Mikail Sayan <msayan@outlook.de>
2022-04-26 14:34:20 +0200

    Add Image

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
-import { Typography } from "@mui/material";
+import { Container, Grid, Typography } from "@mui/material";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return ( 
     <>
-      <Typography variant="h1">Home</Typography>
-      <Link href="/create">Create a new card</Link>
+      <Container maxWidth="xl">
+        <Grid container spacing={4}>
+            <Grid item xs={12}>
+              <Typography variant="h1">Home</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Link href="/create">Create a new card</Link>
+            </Grid>
+            <Grid item xs={12}>
+              <Image src="/lean-coffee.jpg" alt="Lean Coffee" width={640} height={428} layout="responsive"></Image>
+            </Grid>
+        </Grid>
+      </Container>
     </>
   )
 }

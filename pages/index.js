6cabe71ad75a41f6fe2bda371d7d808a7ef970ab
Mikail Sayan <msayan@outlook.de>
2022-04-26 12:56:59 +0200

    Add typography

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
+import { Typography } from "@mui/material";
 import Link from "next/link";
 
 export default function Home() {
   return ( 
     <>
-      <h1>Home</h1>
+      <Typography variant="h1">Home</Typography>
       <Link href="/create">Create a new card</Link>
     </>
   )
